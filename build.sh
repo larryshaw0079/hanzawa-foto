452b416a10b00b1a32a8bb14ab2ee309a2db17c3
xiaoqinfeng <larryshaw0079@live.com>
2024-02-25 20:15:53 +0800

    update

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 rm -rf images/thumbs/*
 for i in $(ls images/fulls/*)
 do
-convert ${i} -resize 640x360 images/thumbs/$(basename ${i})
+convert ${i} -resize 960x480 images/thumbs/$(basename ${i})
 done
 git add .
 git commit -m "update"
